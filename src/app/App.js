274e98c84d4fb25c7f87c1f19c2ae5887a15b542
Zihao Wang <416990273@qq.com>
2018-09-03 11:36:13 +0800

    1.tab navi 添加icon

--- src/app/App.js
+++ src/app/App.js
@@ -3,27 +3,12 @@ import { StyleSheet } from 'react-native';
 import { createBottomTabNavigator } from 'react-navigation';
 import { MarketStack, TradeStack, NewsStack, MineStack } from '../global/register_screens';
 
-// TabNavigator.navigationOptions = ({ navigation }) => {
-//   let { routeName } = navigation.state.routes[navigation.state.index];
-
-//   // You can do whatever you like here to pick the title based on the route name
-//   let headerTitle = routeName;
-
-//   return {
-//     headerTitle,
-//   };
-// };
-
-export default createBottomTabNavigator(
-  {
+export default createBottomTabNavigator({
     MarketStack,
     TradeStack,
     NewsStack,
     MineStack
-  },{
-    navigationOptions: {title: null},
-  }
-);
+  });
 
 const styles = StyleSheet.create({
   container: {
--- src/global/register_screens.js
+++ src/global/register_screens.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import { Image } from 'react-native';
 import { createStackNavigator } from 'react-navigation';
 import { TAB_NAVI_NAME } from './config';
 import MarketScreen from '../screens/market';
@@ -11,7 +13,8 @@ let MarketStack = createStackNavigator({ MarketScreen }, {  //行情
   }
 });
 MarketStack.navigationOptions = {
-  tabBarLabel: TAB_NAVI_NAME[0]
+  tabBarLabel: TAB_NAVI_NAME[0],
+  tabBarIcon: <Image source={require('../image/test_default.png')} />
 };
 let TradeStack = createStackNavigator({ TradeScreen }, {    //模拟交易
   navigationOptions: {
@@ -19,7 +22,8 @@ let TradeStack = createStackNavigator({ TradeScreen }, {    //模拟交易
   }
 });
 TradeStack.navigationOptions = {
-  tabBarLabel: TAB_NAVI_NAME[1]
+  tabBarLabel: TAB_NAVI_NAME[1],
+  tabBarIcon: <Image source={require('../image/test_default.png')} />
 };
 let NewsStack = createStackNavigator({ NewsScreen }, {      //资讯
   navigationOptions: {
@@ -27,7 +31,8 @@ let NewsStack = createStackNavigator({ NewsScreen }, {      //资讯
   }
 });
 NewsStack.navigationOptions = {
-  tabBarLabel: TAB_NAVI_NAME[2]
+  tabBarLabel: TAB_NAVI_NAME[2],
+  tabBarIcon: <Image source={require('../image/test_default.png')} />
 };
 let MineStack = createStackNavigator({ MineScreen }, {      //我的
   navigationOptions: {
@@ -35,7 +40,8 @@ let MineStack = createStackNavigator({ MineScreen }, {      //我的
   }
 });
 MineStack.navigationOptions = {
-  tabBarLabel: TAB_NAVI_NAME[3]
+  tabBarLabel: TAB_NAVI_NAME[3],
+  tabBarIcon: <Image source={require('../image/test_default.png')} />
 };
 
 export {
