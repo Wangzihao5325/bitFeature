d3bde40636fe4f8dfd7fa01872555a3b0d392545
Zihao Wang <416990273@qq.com>
2018-09-13 15:38:05 +0800

    更新充值页面

--- src/components/UnderlineInput.js
+++ src/components/UnderlineInput.js
@@ -1,24 +1,68 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import PropTypes from 'prop-types';
 import RNTextInput from 'react-native-text-input-enhance';
 import VectorIconBtn from './IconBtn';
+import { DEVICE_WIDTH } from '../global/config';
+import CommonStyles from '../global/common_styles';
 export default class UnderlineInput extends Component {
+  constructor() {
+    super();
+    this._clear = this._clear.bind(this);
+    this._onChangeText = this._onChangeText.bind(this);
+  }
+  static propTypes = {
+    fitMode: PropTypes.bool,
+    title: PropTypes.string,
+    onChangeText: PropTypes.func,
+  }
+  static defaultProps = {
+    fitMode: false
+  }
   _clear() {
     this.textInputRef.clear();
   }
   _onChangeText(text) {
-
+    if (this.props.onChangeText) {
+      this.props.onChangeText(text);
+    }
   }
   render() {
     return (
-      <View >
-        <View>
-          <Text>{this.props.title}</Text>
-          <RNTextInput hasRef={ref => (this.textInputRef = ref)} onChangeText={this._onChangeText.bind(this)} maxLength={11} style={{ height: 36, width: 200, color: 'white' }} />
-          <VectorIconBtn name='close' onPress={() => this._clear()} />
+      <View style={this.props.fitMode ? styles.fitContainer : styles.unfitContainer}>
+        <View style={[CommonStyles.innerLineCenterStyle, styles.inputView, { borderBottomColor: '#2C5387', borderBottomWidth: 1 }]}>
+          <View style={[CommonStyles.innerAbsCenterStyle, styles.headerTitleContainer]}>
+            <Text style={{ fontSize: 20, color: '#909090', fontWeight: 'bold' }}>{this.props.title}</Text>
+          </View>
+          <RNTextInput hasRef={ref => (this.textInputRef = ref)} onChangeText={this._onChangeText} maxLength={11} style={{ height: 36, width: 200, color: 'white' }} />
+          <VectorIconBtn name='close' onPress={this._clear} />
         </View>
-        {this.props.addBottom ? <View /> : null}
+        {this.props.fitMode ? <View style={styles.fitView} /> : null}
       </View>
     );
   }
-} 
+}
+
+const styles = StyleSheet.create({
+  fitContainer: {
+    width: DEVICE_WIDTH,
+    height: 50 + 20,
+    paddingHorizontal: 10,
+  },
+  unfitContainer: {
+    width: DEVICE_WIDTH,
+    height: 50,
+    paddingHorizontal: 10,
+  },
+  inputView: {
+    flex: 5,
+    justifyContent: 'space-between',
+  },
+  headerTitleContainer: {
+    height: 24,
+    width: 90
+  },
+  fitView: {
+    flex: 2
+  }
+});
--- src/screens/mine/login/recharge/index.js
+++ src/screens/mine/login/recharge/index.js
@@ -2,8 +2,12 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { connect } from 'react-redux'
 import CardBalance from '../../../../components/CardHeader/CardBalance';
-import { TAB_NAVI_HEADER_BGCOLOR, HEADER_TINT_COLOR, SCREEN_BGCOLOR } from '../../../../global/config';
+import UnderlineInput from '../../../../components/UnderlineInput';
+import NormalBtn from '../../../../components/NormalBtn';
+import { TAB_NAVI_HEADER_BGCOLOR, HEADER_TINT_COLOR, SCREEN_BGCOLOR, DEVICE_WIDTH, BTN_BGCOLOR_RED, BRIGHT_BLUE } from '../../../../global/config';
 import { I_WILL_RECHARGE } from '../../../../global/I18n';
+import CommonStyles from '../../../../global/common_styles';
+
 class RechargeScreen extends Component {
   static navigationOptions = {
     title: I_WILL_RECHARGE,
@@ -12,11 +16,36 @@ class RechargeScreen extends Component {
     },
     headerTintColor: HEADER_TINT_COLOR
   }
-
+  constructor(props) {
+    super(props);
+    this._onChangeText = this._onChangeText.bind(this);
+  }
+  state = {
+    addedBalance: this.props.balance
+  }
+  _onChangeText = (text) => {
+    let num = parseFloat(text);
+    this.setState((prevState, props) => {
+      return ({
+        addedBalance: props.balance + num
+      });
+    });
+  }
   render() {
     return (
       <View style={{ flex: 1, backgroundColor: SCREEN_BGCOLOR }}>
-        <CardBalance />
+        <View style={[{ height: 120, width: DEVICE_WIDTH }, CommonStyles.innerAbsCenterStyle]}>
+          <CardBalance />
+        </View>
+        <View style={{ flex: 1, borderTopColor: '#000000', borderTopWidth: 1 }}>
+          <UnderlineInput fitMode={true} title='充值金额' onChangeText={this._onChangeText} />
+          <NormalBtn title='立即充值' style={{ backgroundColor: BTN_BGCOLOR_RED, height: 45, width: DEVICE_WIDTH - 20, alignSelf: 'center', borderRadius: 5 }} titleStyle={{ color: 'white' }} />
+          <View style={{ display: 'flex', justifyContent: 'flex-end', paddingTop: 10, paddingRight: 10, flexDirection: 'row' }}>
+            <Text style={{ color: '#909090' }}>充值后余额</Text>
+            <Text style={{ color: BRIGHT_BLUE }}>{this.state.addedBalance}</Text>
+            <Text style={{ color: '#909090' }}>元</Text>
+          </View>
+        </View>
       </View>
     );
   }
