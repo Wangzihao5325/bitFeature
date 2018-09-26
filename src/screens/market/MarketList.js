1d1a875b57216726f8bb17dc35f824bb357ec3bf
Zihao Wang <416990273@qq.com>
2018-09-26 14:48:39 +0800

    1.行情列表显示优化

--- src/screens/market/MarketList.js
+++ src/screens/market/MarketList.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import { View, TouchableHighlight, Text, FlatList, StyleSheet } from 'react-native';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import _ from 'lodash';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import { rateStrGenerator, priceStrGenerator } from '../../global/util/index';
-import { DEVICE_WIDTH, HEADER_TITLE_BGCOLOR } from '../../global/config';
+import { contractMap2Config } from '../../global/commodity_list';
+import { rateStrGenerator, priceStr2Generator } from '../../global/util/index';
+import { DEVICE_WIDTH, UP_TEXT_COLOR, DOWN_TEXT_COLOR } from '../../global/config';
 import CommonStyles from '../../global/common_styles';
 const NORMAL_TEXTCOLOR = '#7E829B';
 const NORMAL_BACKGROUNDCOLOR = '#20212A';
 const FONT_SIZE = 18;
+const DEFAULT_DOT_SIZES = 2;
 class ListHeader extends Component {
   render() {
     return (
@@ -46,20 +49,58 @@ class ListHeader extends Component {
   }
 }
 class ItemComponent extends Component {
+  static propTypes = {
+    contractNo: PropTypes.string,
+    price: PropTypes.number,
+    changeRate: PropTypes.number,
+    changeNum: PropTypes.number,
+    tradeValue: PropTypes.number,
+    holdValue: PropTypes.number,
+    isRate: PropTypes.bool,
+    isTradeValue: PropTypes.bool,
+  }
+  static defaultProps = {
+    isRate: true,
+    isTradeValue: true
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextProps.price !== this.props.price) {
+      return true;
+    }
+    if (nextProps.isTradeValue !== this.props.isTradeValue) {
+      return true;
+    }
+    if (nextProps.isRate !== this.props.isRate) {
+      return true;
+    }
+    if (this.props.isTradeValue) {
+      if (nextProps.tradeValue !== this.props.tradeValue) {
+        return true;
+      }
+    } else {
+      if (nextProps.holdValue !== this.props.holdValue) {
+        return true;
+      }
+    }
+    return false;
+  }
   render() {
-    // let priceText = priceStrGenerator(this.props.price, dotSize, this.props.changeRate);
-    // let changeText = this.props.isRate ? rateStrGenerator(this.props.changeRate, DEFAULT_DOT_SIZES, true) : rateStrGenerator(this.props.changeNum, dotSize, false);
-    // let valueText = this.props.isTradeValue ? this.props.tradeValue : this.props.holdValue;
+    let dotSize = contractMap2Config[this.props.contractNo].dotSize;
+    let contractName = contractMap2Config[this.props.contractNo].fullName;
+    let priceText = priceStr2Generator(this.props.price, dotSize);
+    let changeText = this.props.isRate ? rateStrGenerator(this.props.changeRate, DEFAULT_DOT_SIZES, true) : rateStrGenerator(this.props.changeNum, dotSize, false);
+    let valueText = this.props.isTradeValue ? this.props.tradeValue : this.props.holdValue;
+    let colorStyle = this.props.changeRate >= 0 ? { color: UP_TEXT_COLOR } : { color: DOWN_TEXT_COLOR };
     return (
-      <View style={[{ height: 61, width: DEVICE_WIDTH, backgroundColor: NORMAL_BACKGROUNDCOLOR, borderBottomColor: 'black', borderBottomWidth: StyleSheet.hairlineWidth }, CommonStyles.innerLineCenterStyle]}>
+      <View style={[{ height: 61, width: DEVICE_WIDTH, display: 'flex', backgroundColor: NORMAL_BACKGROUNDCOLOR, borderBottomColor: 'black', borderBottomWidth: StyleSheet.hairlineWidth }, CommonStyles.innerLineCenterStyle]}>
         <View style={{ flex: 4, paddingLeft: 10 }}>
-          <Text style={{ color: 'white' }}>{/*this.props.contractName*/'黄金1808'}</Text>
-          <Text style={{ color: NORMAL_TEXTCOLOR }}>{/*this.props.contractNo*/'GC1808'}</Text>
+          <Text style={{ color: 'white', fontSize: 18 }}>{contractName}</Text>
+          <Text style={{ color: NORMAL_TEXTCOLOR }}>{this.props.contractNo}</Text>
         </View>
         <View style={[{ flex: 6 }, CommonStyles.innerLineCenterStyle]}>
-          <View style={CommonStyles.absoluateCenterStyle}><Text style={{ color: 'white' }}>{/*priceText*/3120}</Text></View>
-          <View style={CommonStyles.absoluateCenterStyle}><Text style={{ color: 'white' }}>{/*changeText*/6766}</Text></View>
-          <View style={CommonStyles.absoluateCenterStyle}><Text style={{ color: 'white' }}>{/*valueText*/1.05}</Text></View>
+          <View style={[CommonStyles.innerAbsCenterStyle, styles.partContainer]}><Text style={colorStyle}>{priceText}</Text></View>
+          <View style={[CommonStyles.innerAbsCenterStyle, styles.partContainer]}><Text style={{ color: 'white' }}>{valueText}</Text></View>
+          <View style={[CommonStyles.innerAbsCenterStyle, styles.partContainer]}><Text style={colorStyle}>{changeText}</Text></View>
         </View>
       </View>
     );
@@ -76,7 +117,17 @@ class MarkList extends Component {
         <ListHeader />
         <FlatList
           data={data}
-          renderItem={({ item }) => <ItemComponent key={item.contract_name} />}
+          renderItem={
+            ({ item }) => <ItemComponent
+              key={item.contract_name}
+              contractNo={item.contract_name}
+              price={item.last}
+              changeRate={item.change_rate}
+              changeNum={item.change_value}
+              tradeValue={item.volume}
+              holdValue={item.position}
+            />
+          }
         />
       </View>
     );
