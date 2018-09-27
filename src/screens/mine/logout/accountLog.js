ff6ec606195e92b072f49813e96d7b11b3c1448a
Zihao Wang <416990273@qq.com>
2018-09-27 15:26:07 +0800

    1.调整NormalInput样式2.修改登陆页面

--- src/screens/mine/logout/accountLog.js
+++ src/screens/mine/logout/accountLog.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import store from '../../../store/index';
+import { action_custom_service_model_show } from '../../../store/actions/customServiceAction';
 import { action_getbalancerate, action_login } from '../../../store/actions/accountAction';
 import Api from '../../../socket/platform/api';
 import NormalInput from '../../../components/NormalInput';
@@ -9,6 +10,7 @@ import VectorIconBtn from '../../../components/IconBtn';
 import Variables from '../../../global/Variables';
 import { TAB_NAVI_HEADER_BGCOLOR, HEADER_TINT_COLOR, BTN_BGCOLOR_RED, SCREEN_BGCOLOR, DEVICE_WIDTH } from '../../../global/config';
 
+const NORMAL_BACKGROUNDCOLOR = '#20212A';
 let reg = { accountInput: '', passwordInput: '' };
 export default class AccountLogScreen extends Component {
   static navigationOptions = ({ navigation }) => {
@@ -31,7 +33,7 @@ export default class AccountLogScreen extends Component {
     this.props.navigation.setParams({ customService: this._customService });
   }
   _customService = () => {
-    console.log('customService');
+    store.dispatch(action_custom_service_model_show());
   }
   _getbalancerateSuccess = (result) => {
     store.dispatch(action_getbalancerate(result));
@@ -56,13 +58,13 @@ export default class AccountLogScreen extends Component {
   }
   render() {
     return (
-      <View style={{ flex: 1, backgroundColor: SCREEN_BGCOLOR }}>
-        <NormalInput onChangeText={this._accountChange} style={{ marginTop: 20 }} headerTitle='手机' tips='请输入正确手机号码' />
-        <NormalInput onChangeText={this._passwordChange} headerTitle='密码' tips='密码由6-16位数字和字母组成' />
+      <View style={{ flex: 1, backgroundColor: NORMAL_BACKGROUNDCOLOR }}>
+        <NormalInput secureTextEntry={false} onChangeText={this._accountChange} style={{ marginTop: 20 }} headerTitle='手机' tips='请输入正确手机号码' />
+        <NormalInput secureTextEntry={true} onChangeText={this._passwordChange} style={{ marginTop: 20 }} headerTitle='密码' tips='密码由6-16位数字和字母组成' />
         <NormalBtn
           disabled={false}
           title='登陆'
-          style={{ height: 45, width: DEVICE_WIDTH - 20, backgroundColor: BTN_BGCOLOR_RED, alignSelf: 'center' }}
+          style={{ height: 45, width: DEVICE_WIDTH - 20, backgroundColor: BTN_BGCOLOR_RED, alignSelf: 'center', marginTop: 40 }}
           titleStyle={{ color: 'white' }}
           unableStyle={{ backgroundColor: '#909090', height: 45, width: DEVICE_WIDTH - 10 }}
           onPress={this._login}
