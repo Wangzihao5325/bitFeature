2f5c9a3a927d6bcf11dd4135447d85e82d4fa1ec
Zihao Wang <416990273@qq.com>
2018-09-30 11:01:18 +0800

    1.修改手机号页面

--- src/screens/mine/login/bindPhone/index.js
+++ src/screens/mine/login/bindPhone/index.js
@@ -1,6 +1,38 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TextInput } from 'react-native';
 import { TAB_NAVI_HEADER_BGCOLOR, HEADER_TINT_COLOR, DEVICE_WIDTH } from '../../../../global/config';
+import VectorIconBtn from '../../../../components/IconBtn';
+import NormalBtn from '../../../../components/NormalBtn';
+import VerificationCode from '../../../../components/VerificationCode';
+import Variables from '../../../../global/Variables';
+const NORMAL_TEXTCOLOR = '#7E829B';
+const DARKER_BGCOLOR = '#17191E';
+const NORMAL_BACKGROUNDCOLOR = '#20212A';
+const NORMAL_COMPONENT_BACKGROUNDCOLOR = '#323442';
+const HIGHLIGHT_BGCOLOR = '#FED330';
+class SecurityItem extends Component {
+  state = {
+    security: true,
+    iconName: 'eye'
+  }
+  _changeSecurity = () => {
+    this.setState((preState, props) => {
+      let name = preState.iconName === 'eye' ? 'eye-slash' : 'eye';
+      return {
+        security: !preState.security,
+        iconName: name
+      }
+    })
+  }
+  render() {
+    return (
+      <View style={{ height: 60, width: DEVICE_WIDTH, display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+        <View style={{ height: 40, width: 0.25 * DEVICE_WIDTH, alignItems: 'center', flexDirection: 'row-reverse' }}><Text style={{ fontSize: 18, marginRight: 10, color: NORMAL_TEXTCOLOR }}>{this.props.title}</Text></View>
+        <View style={{ height: 40, width: 0.75 * DEVICE_WIDTH - 10, alignItems: 'center', flexDirection: 'row', backgroundColor: DARKER_BGCOLOR, borderRadius: 5 }}><TextInput onChangeText={this.props.onChangeText} secureTextEntry={this.state.security} style={{ flex: 1, color: 'white', fontSize: 18, marginLeft: 10 }} /><VectorIconBtn color={NORMAL_TEXTCOLOR} name={this.state.iconName} onPress={this._changeSecurity} /></View>
+      </View>
+    );
+  }
+}
 export default class BindPhoneScreen extends Component {
   static navigationOptions = {
     title: "修改手机号",  //header标题
@@ -10,10 +42,53 @@ export default class BindPhoneScreen extends Component {
     },
     headerTintColor: HEADER_TINT_COLOR
   };
+  _changeMobile = () => {
+
+  }
+  _codeOldTextChange = () => {
+
+  }
+  _codeNewTextChange = () => {
+
+  }
+  _getMessageCodeOld = () => {
+
+  }
+  _getMessageCodeNew = () => {
+
+  }
+  _newMobileTextChange = () => {
+
+  }
   render() {
+    let phoneNum = Variables.account.mobileAccount.concat();
+    let partA = phoneNum.slice(0, 3);
+    let partB = phoneNum.slice(7);
+    let showPhoneNum = partA + '****' + partB;
     return (
-      <View style={{ flex: 1 }}>
-        <Text>BindPhone</Text>
+      <View style={{ flex: 1, backgroundColor: NORMAL_BACKGROUNDCOLOR }}>
+        <View>
+          {/*原手机号*/}
+          <View style={{ height: 60, width: DEVICE_WIDTH, display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+            <View style={{ height: 40, width: 0.25 * DEVICE_WIDTH, alignItems: 'center', flexDirection: 'row-reverse' }}><Text style={{ fontSize: 18, marginRight: 10, color: NORMAL_TEXTCOLOR }}>原手机号</Text></View>
+            <View style={{ height: 40, width: 0.75 * DEVICE_WIDTH - 10, alignItems: 'center', flexDirection: 'row', backgroundColor: NORMAL_COMPONENT_BACKGROUNDCOLOR, borderRadius: 5 }}><Text style={{ fontSize: 20, marginLeft: 10, color: 'white' }}>{showPhoneNum}</Text></View>
+          </View>
+          {/*验证码*/}
+          <VerificationCode onChangeText={this._codeOldTextChange} getMessageCode={this._getMessageCodeOld} />
+          {/*新手机号*/}
+          <SecurityItem title={'新手机号'} onChangeText={this._newMobileTextChange} />
+          {/*验证码*/}
+          <VerificationCode onChangeText={this._codeNewTextChange} getMessageCode={this._getMessageCodeNew} />
+          {/*确认按钮*/}
+          <NormalBtn
+            disabled={false}
+            title='确认修改'
+            style={{ height: 45, width: DEVICE_WIDTH - 20, backgroundColor: HIGHLIGHT_BGCOLOR, alignSelf: 'center', marginTop: 40 }}
+            titleStyle={{ color: 'black' }}
+            unableStyle={{ backgroundColor: '#909090', height: 45, width: DEVICE_WIDTH - 10 }}
+            onPress={this._changeMobile}
+          />
+        </View>
       </View>
     );
   }
