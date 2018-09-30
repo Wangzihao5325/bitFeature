a4e62002e0a083427257d4b9a9a8ed32fe83705e
Zihao Wang <416990273@qq.com>
2018-09-30 11:44:58 +0800

    1.重构

--- src/screens/mine/login/recharge/index.js
+++ src/screens/mine/login/recharge/index.js
@@ -5,6 +5,7 @@ import store from '../../../../store/index';
 import { action_custom_service_model_show } from '../../../../store/actions/customServiceAction';
 import VectorIconBtn from '../../../../components/IconBtn';
 import NormalBtn from '../../../../components/NormalBtn';
+import MaterialInput from '../../../../components/MaterialInput';
 import { TAB_NAVI_HEADER_BGCOLOR, HEADER_TINT_COLOR, SCREEN_BGCOLOR, DEVICE_WIDTH, BTN_BGCOLOR_RED, BRIGHT_BLUE } from '../../../../global/config';
 import { I_WILL_RECHARGE, RECHARGE_NUM, RECHARGE_RIGHT_NOW, AFTER_RECHARGE_NUM, YEN } from '../../../../global/I18n';
 import CommonStyles from '../../../../global/common_styles';
@@ -62,12 +63,7 @@ class RechargeScreen extends Component {
           </View>
         </View>
         {/*金额输入*/}
-        <View style={{ height: 60, width: DEVICE_WIDTH, backgroundColor: NORMAL_BACKGROUNDCOLOR, display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-          <View style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', marginVertical: 4 }}>
-            <Text style={{ color: NORMAL_TEXTCOLOR, marginLeft: 10, fontSize: 20 }}>充值金额</Text>
-          </View>
-          <View style={{ flex: 1, height: 40, marginLeft: 10, marginRight: 15, backgroundColor: DARK_BGCOLOR, flexDirection: 'row', borderRadius: 5, alignItems: 'center' }}><TextInput style={{ flex: 1, color: 'white' }} onChangeText={this._onChangeText} /><Text style={{ color: NORMAL_TEXTCOLOR, fontSize: 20, marginRight: 5 }}>元</Text></View>
-        </View>
+        <MaterialInput title='充值金额' onChangeText={this._onChangeText} footerStr='元' />
         {/*充值后余额*/}
         <View style={{ height: 60, width: DEVICE_WIDTH, backgroundColor: NORMAL_BACKGROUNDCOLOR, display: 'flex', justifyContent: 'center', marginTop: 10 }}>
           <View style={{ display: 'flex', flexDirection: 'row', alignItems: 'baseline', marginVertical: 4 }}>
@@ -81,7 +77,12 @@ class RechargeScreen extends Component {
     );
   }
 }
-
+{/* <View style={{ height: 60, width: DEVICE_WIDTH, backgroundColor: NORMAL_BACKGROUNDCOLOR, display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+<View style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', marginVertical: 4 }}>
+  <Text style={{ color: NORMAL_TEXTCOLOR, marginLeft: 10, fontSize: 20 }}>充值金额</Text>
+</View>
+<View style={{ flex: 1, height: 40, marginLeft: 10, marginRight: 15, backgroundColor: DARK_BGCOLOR, flexDirection: 'row', borderRadius: 5, alignItems: 'center' }}><TextInput style={{ flex: 1, color: 'white' }} onChangeText={this._onChangeText} /><Text style={{ color: NORMAL_TEXTCOLOR, fontSize: 20, marginRight: 5 }}>元</Text></View>
+</View> */}
 function mapState2Props(store) {
   return {
     balance: store.account.balance
