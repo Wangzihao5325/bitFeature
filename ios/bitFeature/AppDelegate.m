beee9ed47bfd4c0066e8a6a235c4472910ebf62b
Zihao Wang <416990273@qq.com>
2018-11-20 09:10:36 +0800

    1.iOS bundle 包更新

--- ios/bitFeature/AppDelegate.m
+++ ios/bitFeature/AppDelegate.m
@@ -17,7 +17,7 @@
   NSURL *jsCodeLocation;
 
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
-//  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"index.ios" withExtension:@"jsbundle"];
+//  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"index" withExtension:@"jsbundle"];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"bitFeature"
                                                initialProperties:nil
