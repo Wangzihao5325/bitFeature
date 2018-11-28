c4d1ee7d68bdf1557716d4d49bfff78812e4a146
Zihao Wang <416990273@qq.com>
2018-11-28 15:16:21 +0800

    1.优化综合操盘细则

--- src/screens/trade/operateDetails/index.js
+++ src/screens/trade/operateDetails/index.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView, StyleSheet } from 'react-native';
 import VectorIconBtn from '../../../components/IconBtn';
-import { TAB_NAVI_HEADER_BGCOLOR, HEADER_TINT_COLOR } from '../../../global/config';
+import store from '../../../store/index';
+import { action_custom_service_model_show } from '../../../store/actions/customServiceAction';
+import { TAB_NAVI_HEADER_BGCOLOR, HEADER_TINT_COLOR, DEVICE_WIDTH } from '../../../global/config';
+
+const NORMAL_BACKGROUNDCOLOR = '#20212A';
+const NORMAL_TEXTCOLOR = '#7E829B';
 export default class OperateDetailsScreen extends Component {
   static navigationOptions = ({ navigation }) => {
     return {
@@ -14,42 +19,56 @@ export default class OperateDetailsScreen extends Component {
       headerTintColor: HEADER_TINT_COLOR
     }
   };
+  componentDidMount() {
+    this.props.navigation.setParams({ customService: this._customService });
+  }
+  _customService = () => {
+    store.dispatch(action_custom_service_model_show(this.props.navigation));
+  }
   render() {
     return (
-      <View>
-        <View><Text>操盘流程</Text></View>
+      <ScrollView style={{ width: DEVICE_WIDTH, height: 900, backgroundColor: NORMAL_BACKGROUNDCOLOR }}>
+        <View><Text style={styles.titleStyle}>操盘流程</Text></View>
         <View>
-          <Text>①选择方案→②支付保证金→③获取操盘账号→④在指定软件里操盘→⑤终结方案，结算到账</Text>
+          <Text style={styles.normalTextStyle}>①选择方案→②支付保证金→③获取操盘账号→④在指定软件里操盘→⑤终结方案，结算到账</Text>
         </View>
-        <View><Text>操盘合约</Text></View>
+        <View><Text style={styles.titleStyle}>交易时间</Text></View>
         <View>
-          <Text>克同时操盘17种期货的当前主力合约，分别为：富时A50、恒指期货、国际原油、迷你纳指、迷你道指、迷你标普、德国DAX、日经225、小恒指、美黄金、H股指数、小H股指数、美铜、美白银、小原油、迷你德国DAX指数、天然气。</Text>
+          <Text style={styles.normalTextStyle}>所有种类的期货交易时间基本与交易所同步。由于风控限制，分别于交易所收市时间提前5分钟执行强平；因新加坡交易所的特殊交易规则，富时A50、日经225需在收市前10分钟执行强平。不同期货品种具体交易时间段以方案申请页公示为准。</Text>
         </View>
-        <View><Text>交易时间</Text></View>
+        <View><Text style={styles.titleStyle}>操盘保证金</Text></View>
         <View>
-          <Text>所有种类的期货交易时间基本与交易所同步。由于风控限制，分别于交易所收市时间提前5分钟执行强平；因新加坡交易所的特殊交易规则，富时A50、日经225需在收市前10分钟执行强平。不同期货品种具体交易时间段以方案申请页公示为准。</Text>
+          <Text style={styles.normalTextStyle}>保证金越多，可同时交易的期货品种越多，可持仓手数也越多；结束时若亏损，用保证金赔付，若无亏损，则全额退还。</Text>
         </View>
-        <View><Text>操盘保证金</Text></View>
+        <View><Text style={styles.titleStyle}>总操盘资金</Text></View>
         <View>
-          <Text>保证金越多，可同时交易的期货品种越多，可持仓手数也越多；结束时若亏损，用保证金赔付，若无亏损，则全额退还。</Text>
+          <Text style={styles.normalTextStyle}>总操盘资金=操盘保证金+平台授信金额</Text>
         </View>
-        <View><Text>总操盘资金</Text></View>
+        <View><Text style={styles.titleStyle}>亏损平仓线</Text></View>
         <View>
-          <Text>总操盘资金=操盘保证金+平台授信金额</Text>
+          <Text style={styles.normalTextStyle}>总操盘资金低于亏损平仓线时，系统将自动平仓；低于平仓线后无法开仓，需要追加保证金到平仓线以上才可继续交易。</Text>
         </View>
-        <View><Text>亏损平仓线</Text></View>
+        <View><Text style={styles.titleStyle}>账户管理费</Text></View>
         <View>
-          <Text>总操盘资金低于亏损平仓线时，系统将自动平仓；低于平仓线后无法开仓，需要追加保证金到平仓线以上才可继续交易。</Text>
+          <Text style={styles.normalTextStyle}>目前黄金期货通不收取账户管理费</Text>
         </View>
-        <View><Text>账户管理费</Text></View>
+        <View><Text style={styles.titleStyle}>交易手续费</Text></View>
         <View>
-          <Text>目前黄金期货通不收取账户管理费</Text>
+          <Text style={styles.normalTextStyle}>每手开仓、平仓的手续费，不同期货品种具体手续费不同，由交易软件直接扣取。</Text>
         </View>
-        <View><Text>交易手续费</Text></View>
-        <View>
-          <Text>每手开仓、平仓的手续费，不同期货品种具体手续费不同，由交易软件直接扣取。</Text>
-        </View>
-      </View>
+      </ScrollView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  titleStyle: {
+    fontSize: 18,
+    color: 'white',
+    marginTop: 10,
+    marginBottom: 10
+  },
+  normalTextStyle: {
+    color: NORMAL_TEXTCOLOR
+  }
+});
